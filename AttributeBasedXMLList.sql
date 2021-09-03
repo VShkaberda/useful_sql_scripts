4033914fb1dded888a8fd6d7739255c1f6225ff3
VShkaberda <V.Shkaberda@gmail.com>
2021-09-03 15:00:37 +0300

    Add script to generate and parse attribute based XMLList.

---
+++ AttributeBasedXMLList.sql
@@ -0,0 +1,15 @@
+-- create varchar list from attribute [i] of [table]
+DECLARE @List VARCHAR(MAX), @XMLbuildScript NVARCHAR(MAX)
+
+SELECT @List = STUFF((SELECT ', ' + cast([i] as varchar(10)) FROM [table] FOR XML PATH('')),1,1,'')
+
+-- create attribute based XMLList
+DECLARE @AttributeBasedXMLList XML
+SELECT @XMLBuildScript='SELECT @AttributeBasedXMLList = ( SELECT x AS "@x" FROM (values ('
+      +REPLACE(@List,',','),(')+'))d(x) FOR XML PATH(''y''), ROOT(''root''), TYPE )'
+EXEC sp_executesql @XMLBuildScript, N'@AttributeBasedXMLList XML OUT',
+                    @AttributeBasedXMLList OUT
+
+-- select to parse attribute based XMLList into table
+SELECT x.y.value('.','int')
+FROM @AttributeBasedXMLList.nodes('root/y/@x') AS x( y )
